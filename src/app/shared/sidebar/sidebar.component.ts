389e45a1c8bf090bf8a684bc5be5a99a89ab0421
karovh <carolinavillamilh@gmail.com>
2022-09-13 14:22:45 -0500

    Primer commit

---
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { GifsService } from '../../gifs/services/gifs.service';
+import { ResultadosComponent } from '../../gifs/resultados/resultados.component';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+
+})
+export class SidebarComponent {
+
+  get historial() {
+
+    return this.gifService.historial
+  }
+  constructor(private gifService: GifsService) { }
+
+  buscar(item: string) {
+
+    this.gifService.buscarGifs(item)
+
+  }
+
+
+}
